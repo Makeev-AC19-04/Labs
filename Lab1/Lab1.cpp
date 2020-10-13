83a9589652b7d58bc6c28eb6b8e959e31c636b66
Makeev-AC19-04 <70644348+Makeev-AC19-04@users.noreply.github.com>
2020-10-13 18:20:49 +0300

    Работа над взаимодействием с файлом
    Введена загрузка данных из файла с помощью поиска по id

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <stdlib.h>
 
+
 struct pipe {
     int id;
     float length;
@@ -23,7 +24,7 @@ struct ks {
 pipe createpipe() {
     pipe d;
     int fx;
-    d.id = 0;
+    d.id = rand() % 1000000;
     int inmenup;
     //char filename;
     std::cout << "\nКак вы хотите добавить трубу?\n1. Вручную\n2. Из файла\n3. Отмена\n";
@@ -74,10 +75,10 @@ pipe createpipe() {
     }
 };
 
-    ks createks() {
+ks createks() {
     ks kc;
     int inmenup;
-    kc.id = 0;
+    kc.id = rand() % 1000000;;
     std::cout << "\nКак вы хотите добавить КС?\n1. Ввести вручную\n2. Из файла\n3. Отмена\n";
     std::cin >> inmenup;
     while (1) {
@@ -135,10 +136,14 @@ pipe createpipe() {
 
 int main()
 {
-    int inmenu;
-
+    int inmenu, lastpid, lastksid, wid;
+    int c = 0;
+    std::string currentline;
     pipe p;
     ks k;
+    //std::ifstream fin;
+    //fin.open("data.txt", std::ios::in);
+    //fin.close();
     while (1) {
         std::cout << "\nМеню:\n1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
         std::cin >> inmenu;
@@ -152,7 +157,7 @@ int main()
                         std::cout << "\nТруба сохранена\n";
                         std::ofstream fin;
                         fin.open("data.txt", std::ofstream::out | std::ofstream::app); //https://coderoad.ru/54498954/%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-%D1%84%D0%B0%D0%B9%D0%BB-%D0%B1%D0%B5%D0%B7-%D1%81%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B8%D1%8F-%D1%82%D0%BE%D0%B3%D0%BE-%D1%87%D1%82%D0%BE-%D0%BD%D0%B0%D1%85%D0%BE%D0%B4%D0%B8%D1%82%D1%81%D1%8F-%D0%B2%D0%BD%D1%83%D1%82%D1%80%D0%B8
-                        fin << "\n" << p.length << "\n" << p.diameter << "\n" << p.fix << "\n" << p.id << "\n";
+                        fin << "\n" << "pipe" << "\n" << p.length << "\n" << p.diameter << "\n" << p.fix << "\n" << p.id << "\n";
                         fin.close();
                         break;
                     }
@@ -183,7 +188,7 @@ int main()
                     std::cout << "\nКС сохранена\n";
                     std::ofstream fin;
                     fin.open("data.txt", std::ofstream::out | std::ofstream::app); //https://coderoad.ru/54498954/%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-%D1%84%D0%B0%D0%B9%D0%BB-%D0%B1%D0%B5%D0%B7-%D1%81%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B8%D1%8F-%D1%82%D0%BE%D0%B3%D0%BE-%D1%87%D1%82%D0%BE-%D0%BD%D0%B0%D1%85%D0%BE%D0%B4%D0%B8%D1%82%D1%81%D1%8F-%D0%B2%D0%BD%D1%83%D1%82%D1%80%D0%B8
-                    fin << "\n" << k.name << "\n" << k.effective << "\n" << k.numc << "\n" << k.numcw << "\n" << k.id << "\n";
+                    fin << "\n" << "kc\n"<< k.name << "\n" << k.effective << "\n" << k.numc << "\n" << k.numcw << "\n" << k.id << "\n";
                     fin.close();
                     break;
                 }
@@ -212,6 +217,26 @@ int main()
             }
         }
         else if (inmenu == 3) {
+            std::ifstream fin;
+            fin.open("data.txt", std::ios::in);
+            std::cout << "Трубы:\n";
+            while (getline(fin, currentline)) { //https://ru.stackoverflow.com/questions/258989/%d0%a7%d1%82%d0%b5%d0%bd%d0%b8%d0%b5-%d1%84%d0%b0%d0%b9%d0%bb%d0%b0-%d0%bf%d0%be%d1%81%d1%82%d1%80%d0%be%d1%87%d0%bd%d0%be
+                if (currentline == "pipe") {
+                    fin >> p.length >> p.diameter >> p.fix >> p.id;
+                    std::cout << "\n" << p.length << "\n" << p.diameter << "\n" << p.fix << "\n" << p.id << "\n";
+                }
+            }
+            fin.close();
+            fin.open("data.txt", std::ios::in);
+            std::cout << "\nСтанции:\n";
+            while (getline(fin, currentline)) { 
+                if (currentline == "kc") {
+                    fin >> k.name >> k.numc >> k.numcw >> k.effective >> k.id;
+                    std::cout << "\n" << k.name << "\n" << k.numc << "\n" << k.numcw << "\n" << k.effective << "\n" << k.id << "\n";
+                }
+            }
+            fin.close();
+            //std::cout << c;
         }
         else if (inmenu == 4) {
         }
@@ -220,10 +245,36 @@ int main()
         else if (inmenu == 6) {
         }
         else if (inmenu == 7) {
-            //std::ofstream fin;
-            //fin.open("C:\\Users\\wane2\\Documents\\Задания\\Алгоритмические языки\\Labs\\Lab1\\data.txt");
-            //fin << p.length << "\n" << p.diameter << "\n" << p.fix << "\n" << p.id << "\n" << k.effective << "\n" << k.id << "\n" << k.name << "\n" << k.numc << "\n" << k.numcw;
-            //fin.close();
+            std::cout << "Что вы хотите загрузить?\n1. Трубу\n2. KC\n";
+            std::cin >> inmenu;
+            if (inmenu == 2) {
+                std::cout << "\nВведите id KC:\n";
+                std::cin >> wid;
+                std::ifstream fin;
+                fin.open("data.txt", std::ios::in);
+                while (getline(fin, currentline)) {
+                    if (currentline == "kc") {
+                        fin >> k.name >> k.numc >> k.numcw >> k.effective >> k.id;
+                        if (wid == k.id) {
+                            std::cout << "\n" << k.name << "\n" << k.numc << "\n" << k.numcw << "\n" << k.effective << "\n" << k.id << "\n";
+                        }
+                    }
+                }
+            }
+            else {
+                std::cout << "\nВведите id трубы:\n";
+                std::cin >> wid;
+                std::ifstream fin;
+                fin.open("data.txt", std::ios::in);
+                while (getline(fin, currentline)) {
+                    if (currentline == "pipe") {
+                        fin >> p.length >> p.diameter >> p.fix >> p.id;
+                        if (wid == p.id) {
+                            std::cout << "\n" << p.length << "\n" << p.diameter << "\n" << p.fix << "\n" << p.id << "\n";
+                        }
+                    }
+                }
+            }
         }
         else if (inmenu == 0) {
             return 0;
